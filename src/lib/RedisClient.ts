9aeae4766d9e6646edd259e55cffde77a65f0eb2
mhusnainkhalid72 <l1s22bsse0060@ucp.edu.pk>
2025-09-02 18:24:40 +0500

    final code

--- src/lib/RedisClient.ts
+++ src/lib/RedisClient.ts
@@ -4,14 +4,14 @@ class RedisClient {
   private static instance: Redis;
 
   private constructor() {
-    // Private constructor to prevent instantiation
+    
   }
 
   public static getInstance(): Redis {
     if (!RedisClient.instance) {
       RedisClient.instance = new Redis({
         host: process.env.REDIS_HOST || 'localhost',
-        port: parseInt(process.env.REDIS_PORT || '6380', 10),
+        port: parseInt(process.env.REDIS_PORT || '6379', 10),
         password: process.env.REDIS_PASSWORD || undefined,
         maxRetriesPerRequest: null
       });
